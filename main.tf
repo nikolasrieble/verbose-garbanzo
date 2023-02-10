be7fa8f4ecd770f2c51270aa0bcf25cc85a6bb5a
Miguel Hernández <miguel.hernandez@hashicorp.com>
2023-02-10 14:08:16 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "null_resource" "hello_world" {
+  provisioner "local-exec" {
+    command = "echo hello world!!"
+  }
+}
+
+resource "null_resource" "hello_world_part_2" {
+  provisioner "local-exec" {
+    command = "echo ${var.greetings}!"
+  }
+}
+
+resource "null_resource" "dont_hack_me" {
+  provisioner "local-exec" {
+    command = "echo My password is: ${var.password}"
+  }
+}
+
+module "module-1" {
+  source  = "app.terraform.io/miguels-awesome-org/helloworld/aws"
+  # insert required variables here
+  password = "ZAQ!xsw2"
+}
+
+
