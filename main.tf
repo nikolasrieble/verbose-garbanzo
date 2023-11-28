d50e76c62679f825dab0a45bb853fea724a8c995
Miguel Hernández <miguel.hernandez@hashicorp.com>
2023-11-28 12:21:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,3 +29,16 @@ module "curated-module" {
   # insert required variables here
   password = "ZAQ!xsw2"
 }
+
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_10_minutes" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "10m"
+}
+
+# This resource will create (at least) 10 minutes after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_10_minutes]
+}
