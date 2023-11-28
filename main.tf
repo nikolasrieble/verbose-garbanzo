98a693ba40486f3a5fc81681d8d5c679ac4713b2
Miguel Hernández <miguel.hernandez@hashicorp.com>
2023-11-28 12:53:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,13 +32,13 @@ module "curated-module" {
 
 resource "null_resource" "previous" {}
 
-resource "time_sleep" "wait_3_minutes" {
+resource "time_sleep" "wait_2_minutes" {
   depends_on = [null_resource.previous]
 
-  create_duration = "3m"
+  create_duration = "2m"
 }
 
-# This resource will create (at least) 5 minutes after null_resource.previous
+# This resource will create (at least) 2 minutes after null_resource.previous
 resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_3_minutes]
+  depends_on = [time_sleep.wait_2_minutes]
 }
