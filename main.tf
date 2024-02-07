b16a991b5703b86875ee66f1552754c56a2a9192
Nikolas Rieble <nikolas@rieble.com>
2024-02-07 17:12:18 +0100

    Introduce project_id variable

--- main.tf
+++ main.tf
@@ -66,20 +66,24 @@ terraform {
 
 provider "doormat" {}
 
+variable "project_id" {
+  type = string
+}
+
 data "doormat_gcp_credentials" "creds" {
   provider = doormat
 
   service_account = "nikolasrieble@hc-1eee1e0fde6f4d6cb76f7157200.iam.gserviceaccount.com"
-  project_id      = "tf-onprem-dev"
+  project_id      = variable.project_id
 }
 
 provider "google" {
   access_token = data.doormat_gcp_credentials.creds.access_token
-  project      = "tf-onprem-dev"
+  project      = variable.project_id
 }
 
 module "cloud-storage_example_simple_bucket" {
   source  = "terraform-google-modules/cloud-storage/google//examples/simple_bucket"
   version = "5.0.0"
-  project_id = "tf-onprem-dev"
+  project_id = variable.project_id
 }
