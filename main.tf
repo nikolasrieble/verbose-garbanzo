f8c59b28365901ee766f321a415e4e2b12c006b5
Nikolas Rieble <nikolas@rieble.com>
2024-02-07 14:59:16 +0100

    Provision a simple bucket using doormat provider

--- main.tf
+++ main.tf
@@ -42,3 +42,44 @@ resource "time_sleep" "wait_2_minutes" {
 resource "null_resource" "next" {
   depends_on = [time_sleep.wait_2_minutes]
 }
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+    }
+
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
+    }
+  }
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "nikolasrieble"
+
+    workspaces {
+      name = "verbose-garbanzo"
+    }
+  }
+}
+
+provider "doormat" {}
+
+data "doormat_gcp_credentials" "creds" {
+  provider = doormat
+
+  service_account = "nikolasrieble@hc-1eee1e0fde6f4d6cb76f7157200.iam.gserviceaccount.com"
+  project_id      = "tf-onprem-dev"
+}
+
+provider "google" {
+  access_token = data.doormat_gcp_credentials.creds.access_token
+  project      = "tf-onprem-dev"
+}
+
+module "cloud-storage_example_simple_bucket" {
+  source  = "terraform-google-modules/cloud-storage/google//examples/simple_bucket"
+  version = "5.0.0"
+  project_id = "tf-onprem-dev"
+}
